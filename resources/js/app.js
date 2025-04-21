1395132e4ec0189670e737b52eb750936b25876b
Muhammad Rivaldhi <mhmmdrivaldhi10@gmail.com>
2025-04-21 22:13:13 +0700

    Validate Radius Distance

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
+import Swal from 'sweetalert2';
 import './bootstrap';
+
+function showAlert(type, title, text) {
+    Swal.fire({
+        icon: type,
+        title: title,
+        text: text
+    })
+}
